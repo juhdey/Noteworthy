602d34fa1b5a52490283a80de46fa6c9d1d8653a
juhdey <xunhe@berkeley.edu>
2024-03-18 15:40:20 -0700

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+// App.jsx
 import "./App.css";
 import Button from "./Button.jsx";
+import Image from "./Image.jsx"; // Import your image component
 
 export default function App() {
     return (
-        <div>
+        <div className="app-container">
+            <Image src="./group_photo.jpg" alt="Image" /> {/* Pass the appropriate path and alt text */}
             <header>
                 <Button text="About Us"></Button>
                 <Button text="Our Favorite Video"></Button>
---
+++ src/Image.jsx
@@ -0,0 +1,6 @@
+// Image.jsx
+const Image = ({ src, alt }) => {
+    return <img className="image" src={src} alt={alt} />;
+};
+
+export default Image;
