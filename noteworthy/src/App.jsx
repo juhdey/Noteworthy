2028b179ff809fcd26adab33eea7b41f86ac51df
juhdey <xunhe@berkeley.edu>
2024-03-18 15:00:10 -0700

    first commit

---
+++ noteworthy/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Button from "./Button.jsx";
+
+export default function App() {
+    return (
+        <div>
+            <header>
+                <Button text="About Us"></Button>
+                <Button text="Our Favorite Video"></Button>
+                <Button text="Members"></Button>
+            </header>
+        </div>
+    );
+}
