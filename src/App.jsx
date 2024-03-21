e144f2dcfee3836348aa173ea7cce83e70745eb9
FiftysixTimes7 <25679436+FiftysixTimes7@users.noreply.github.com>
2024-03-20 17:47:56 -0700

    change icon & title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 export default function App() {
     return (
         <div className="app-container">
-            <img className="image" src="group_photo.jpg" alt="Our group photo" />
+            <img className="image" src="src/assets/group_photo.jpg" alt="Our group photo" />
             <header>
                 <a>About Us</a>
                 <a>Our Favorite Video</a>
