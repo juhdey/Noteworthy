aaf1cbf27cbaa11eb8cdd27eb0f1c10ad9b4095f
FiftysixTimes7 <25679436+FiftysixTimes7@users.noreply.github.com>
2024-03-20 17:48:21 -0700

    import shadcn/ui

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+import path from "path"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(dirname(fileURLToPath(import.meta.url)), "./src"),
+    },
+  },
 })
