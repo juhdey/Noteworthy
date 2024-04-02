9cae6c6df8fbe0941001ab6564bfe77b389978da
FiftysixTimes7 <25679436+FiftysixTimes7@users.noreply.github.com>
2024-04-01 21:14:38 -0700

    add a simple intro page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-// import "./App.css";
 import { NavigationBar } from "@/NavigationBar"
+import { Intro } from "@/Intro"
 
 export default function App() {
     return (
         <>
             <NavigationBar />
-            <img className="image" src="src/assets/group_photo.jpg" alt="Our group photo" />
-            <header>
-                <a>About Us</a>
-                <a>Our Favorite Video</a>
-                <a>Members</a>
-            </header>
+            <Intro />
         </>
     );
 }
---
+++ src/Intro.jsx
@@ -0,0 +1,14 @@
+import { Logo } from "@/Logo"
+
+export function Intro() {
+    return <div className="flex h-screen">
+        <img className="inset-y-0 left-0 flex-initial h-full" src="src/assets/group_photo.jpg" alt="Our group photo" />
+        <div className="flex">
+            <div className="flex flex-col font-bold place-content-center p-10 text-7xl">
+                <p>Noteworthy</p>
+                <p>A Cappella</p>
+            </div>
+            <Logo className="w-52 h-52 place-self-center" />
+        </div>
+    </div>
+}
