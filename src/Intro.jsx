4f8dda3438f487f1692378388f0a8dade64d1f1e
FiftysixTimes7 <25679436+FiftysixTimes7@users.noreply.github.com>
2024-04-01 22:16:22 -0700

    hide overflow

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,7 +1,7 @@
 import { Logo } from "@/Logo"
 
 export function Intro() {
-    return <div className="flex">
+    return <div className="flex overflow-hidden">
         <img className="inset-y-0 left-0 flex-initial max-w-prose" src="src/assets/group_photo.jpg" alt="Our group photo" />
         <div className="flex">
             <div className="flex flex-col font-bold place-content-center p-10 text-7xl">
