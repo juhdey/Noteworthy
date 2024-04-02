89d133c527ad9006056d67f18dafe49590de4dce
FiftysixTimes7 <25679436+FiftysixTimes7@users.noreply.github.com>
2024-04-01 21:14:30 -0700

    use sticky

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -2,7 +2,7 @@ import { Logo } from "@/Logo"
 
 export function NavigationBar() {
   return (
-    <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow dark:bg-gray-950">
+    <nav className="sticky inset-x-0 top-0 z-50 bg-white shadow dark:bg-gray-950">
       <div className="container px-4 md:px-6">
         <div className="flex h-14 items-center">
           <a className="mr-auto flex items-center gap-2 text-lg font-semibold" href="#">
